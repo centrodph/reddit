a3515256087d3766d6dc5ec140e6738a6bee8b19
Gerardo Perrucci <centrodph@gmail.com>
2019-09-22 09:40:05 -0300

    clean footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,4 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { selectAppVersion, selectRedditLoading } from "selectors";
-import { getRedditPost } from "actions";
-import { Sidebar, Content } from "components";
+import React from "react";
 
 export function Footer({ version }: { version: number }) {
   return (
