48ef7b68ebeb6d44548da02665604e02b9bd1e01
Gerardo Perrucci <centrodph@gmail.com>
2019-09-22 20:49:48 -0300

    selectors

--- src/selectors/reddit.ts
+++ src/selectors/reddit.ts
@@ -1,5 +1,15 @@
 import { RootState } from "store/rootReducer";
+import { RedditReducerType } from "store/reddit/redditReducer";
+import { RedditPost } from "models";
+
+export function selectReddit(state: RootState): RedditReducerType {
+  return state.reddit;
+}
 
 export function selectRedditLoading(state: RootState): boolean {
-  return state.reddit.loading;
+  return selectReddit(state).loading;
+}
+
+export function selectRedditList(state: RootState): RedditPost[] {
+  return selectReddit(state).list;
 }
