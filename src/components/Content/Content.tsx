27556d4419e50901b2fe53bda85bd7aa2ed772d7
Gerardo Perrucci <centrodph@gmail.com>
2019-09-23 23:26:59 -0300

    basic content

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,4 +1,28 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectActive } from "selectors";
+
 export function Content() {
-  return <div className="content">Content</div>;
+  const post = useSelector(selectActive);
+  if (!post)
+    return (
+      <div className="content">
+        <h1>Select content</h1>
+      </div>
+    );
+  const {
+    data: { title, thumbnail }
+  } = post;
+  return (
+    <div className="content">
+      <h1>{title}</h1>
+      {thumbnail && (
+        <img
+          src={thumbnail}
+          alt=""
+          style={{ maxWidth: "100%", margin: "1rem" }}
+        />
+      )}
+    </div>
+  );
 }
